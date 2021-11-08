9c7ccfd7ddf38555740244a7531f8111eb14fc69
Usama Ahmed <usamaahmed212aaa@gmail.com>
2021-11-08 21:45:44 +0600

    #adminbar > .quicklinks > all .adminbar-sub-menu Design

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,13 @@
 "use strict";
 window.onload = function() {
     // menu item active class add or remove
-    const menu_item = document.querySelectorAll("#adminmenu .menu-item");
-    menu_item.forEach( function (element) {
+    const menu_items = document.querySelectorAll("#adminmenu .menu-item");
+    menu_items.forEach( function (element) {
         element.addEventListener('click', function(e) {
             e.stopPropagation();
             e.preventDefault();
             
-            menu_item.forEach(function (element) {
+            menu_items.forEach(function (element) {
                 element.classList.remove('active');
             });
             this.classList.add('active');
