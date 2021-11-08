7fcfeef585f645a1b1a363e0ae423466e932ef3c
Usama Ahmed <usamaahmed212aaa@gmail.com>
2021-11-08 23:09:08 +0600

    #adminmenu and #adminbar Responsive Design |&and&| Admin Menu Dropdown Toggle

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,23 @@
 "use strict";
 window.onload = function() {
-    // menu item active class add or remove
-    const menu_items = document.querySelectorAll("#adminmenu .menu-item");
-    menu_items.forEach( function (element) {
-        element.addEventListener('click', function(e) {
-            e.stopPropagation();
-            e.preventDefault();
-            
-            menu_items.forEach(function (element) {
-                element.classList.remove('active');
-            });
-            this.classList.add('active');
-        });
-    });
-
     // collapse toggle #adminmenu 
+    const adminmenu = document.querySelector("#adminmenu-wrapper");
     const collapse_button = document.querySelector("#menu-collapse");
     collapse_button.addEventListener('click', function(e) {
         e.stopPropagation();
         e.preventDefault();
         
-        const adminmenu = document.querySelector("#adminmenu-wrapper");
         adminmenu.classList.toggle("collapse");
     });
+    
+    // Admin Menu Dropdown Toggle 
+    const menu_button = document.querySelector(".menu-toggle-item");
+    menu_button.addEventListener("click",function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        
+        adminmenu.classList.toggle("open");
+    });
+
 
 }
