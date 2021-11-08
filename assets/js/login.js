027e30edff2183c62fc999f20372145060ae3993
Usama Ahmed <usamaahmed212aaa@gmail.com>
2021-11-08 21:30:06 +0600

    Login Page Design

---
+++ assets/js/login.js
@@ -0,0 +1,21 @@
+"use strict";
+window.addEventListener('DOMContentLoaded', function(event) {
+    // dashicons Toggle
+    const button = document.querySelector("button");
+    button.addEventListener("click", function(e){
+        e.stopPropagation();
+        e.preventDefault();
+    
+        let targetElement = button.firstElementChild;
+        if (targetElement.classList.contains("dashicons-visibility")) {
+            targetElement.classList.remove("dashicons-visibility");
+            targetElement.classList.add("dashicons-hidden");
+            button.parentElement.firstElementChild.setAttribute('type','text')
+        } else {
+            targetElement.classList.remove("dashicons-hidden");
+            targetElement.classList.add("dashicons-visibility");
+            button.parentElement.firstElementChild.setAttribute('type','password');
+        }
+    });
+    
+});
