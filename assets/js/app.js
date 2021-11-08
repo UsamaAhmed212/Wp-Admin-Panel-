dd3bde907dcbdf57c1646f1ddd51f61ec962cce9
Usama Ahmed <usamaahmed212aaa@gmail.com>
2021-11-08 23:14:03 +0600

    #adminmenu Section .collapse Class Toggle Click Event Remove |&and&| #container Section .collapse Class Toggle Click Event Add

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,17 @@
 "use strict";
 window.onload = function() {
-    // collapse toggle #adminmenu 
-    const adminmenu = document.querySelector("#adminmenu-wrapper");
+    // #menu-collapse Click Then #container Section .collapse class toggle   
+    const container = document.querySelector("#container");
     const collapse_button = document.querySelector("#menu-collapse");
     collapse_button.addEventListener('click', function(e) {
         e.stopPropagation();
         e.preventDefault();
         
-        adminmenu.classList.toggle("collapse");
+        container.classList.toggle("collapse");
     });
     
     // Admin Menu Dropdown Toggle 
+    const adminmenu = document.querySelector("#adminmenu-wrapper");
     const menu_button = document.querySelector(".menu-toggle-item");
     menu_button.addEventListener("click",function(e) {
         e.stopPropagation();
